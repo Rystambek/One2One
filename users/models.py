26642db02e0795522cff0eae543fc72ff46cb66e
djumanov <diyorbekjumanov1120@gmail.com>
2023-05-30 19:24:39 +0500

    one to one

--- users/models.py
+++ users/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length=45)
+    last_name  = models.CharField(max_length=45)
+    username   = models.CharField(max_length=45, unique=True)
+    age        = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return self.full_name()
+    
+
+class Contact(models.Model):
+    user = models.OneToOneField(to=User, on_delete=models.CASCADE)
+
+    phone = models.CharField(max_length=45)
+    address = models.CharField(max_length=45)
+    city = models.CharField(max_length=45)
+    
+    
+    def __str__(self):
+        return f"{self.user.full_name()} - {self.phone}"
